ae48a19368b8e73ea4f1fa0a6b6d557bd4855874
Dishalik <30625317+Dishalik@users.noreply.github.com>
2020-08-11 11:32:54 +1000

    updated code

--- Homepage.cs
+++ Homepage.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 namespace Application
 {
-    public class EmptyClass
+    public class Homepage
     {
-        public EmptyClass()
         {
+        // navigate to Time and Material page
+        public void NavigateToTM(IWebDriver driver)
+        {
+            // Navigate to Time and Material page
+            driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a")).Click();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,115 +8,32 @@ namespace first_script
 {
     class MainClass
     {
-        public static void Main(string[] args)
+     
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Industry Connect!");
 
-            // Init and define webdriver
+            // Init and define driver
             IWebDriver driver = new ChromeDriver();
-            // launch TurnUp portal
-            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
-            // Maximize the browser
-            driver.Manage().Window.Maximize();
-            // Find Username textbox and input username
-            IWebElement username = driver.FindElement(By.Id("UserName"));
-            username.SendKeys("hari");
-            // Find Password textbox and input password
-            IWebElement password = driver.FindElement(By.Id("Password"));
-            password.SendKeys("123123");
-            // Find login button and click
-            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
-            loginButton.Click();
-            // Find hello hari hyperlink
-            IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
-            // Validate if the text on the hyperlink is hello Hari
-            if (helloHari.Text == "Hello hari!")
-            {
-                Console.WriteLine("Logged In successfully, test passed");
-            }
-            else
-            {
-                Console.WriteLine("Login failed, test failed");
-            }
 
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            // Login page object init and definition
+            LoginPage loginObj = new LoginPage();
+            loginObj.LoginSteps(driver);
 
-            Console.WriteLine("Check if the user is able to create a new time/ material record successfully with valid details");
+            // Home page object init and definition
+            HomePage homeObj = new HomePage();
+            homeObj.NavigateToTM(driver);
 
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            // TM page object init and definition
+            TMPage tmObj = new TMPage();
+            tmObj.CreateTM(driver);
 
-            driver.FindElement(By.ClassName("dropdown-toggle")).Click();
+            // Edit existing TM test
+            tmObj.EditTM(driver);
 
-            driver.FindElement(By.XPath("//a[text()='Time & Materials']")).Click();
-
-            driver.FindElement(By.XPath("//a[text()='Create New']")).Click();
-
-            /*
-            IWebElement typecode = driver.FindElement(By.XPath("//[@id='TimeMaterialEditForm']/div/div[1]/div/span[1]"));
-            typecode.Click();
-            var selectDropDown = new SelectElement(typecode);
-            selectDropDown.SelectByText("Time");
-            */
-            Console.WriteLine("Successfully Clicked");
-
-            IWebElement code = driver.FindElement(By.Id("Code"));
-            code.SendKeys("13243");
-
-            IWebElement descrpt = driver.FindElement(By.Id("Description"));
-            descrpt.SendKeys("New test case");
-
-            IWebElement price = driver.FindElement(By.ClassName("k-formatted-value"));
-            price.SendKeys("120.00");
-
-            driver.FindElement(By.Id("SaveButton")).Click();
-            Console.WriteLine("Saved button clicked");
-
-            IWebElement working = driver.FindElement(By.Id("loader"));
-            Console.WriteLine(working.Displayed);
-
-            Console.Write("New Record is Created");
-
-            
-
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-            Console.WriteLine("Check if the user is able to edit an existing time/ material record successfully with valid details");
-
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-            Thread.Sleep(2000);
-            //driver.FindElement(By.ClassName("k-button")).Click();
-            driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[1]/td[5]/a[1]")).Click();
-
-            Thread.Sleep(2000);
-            driver.FindElement(By.Id("Code")).Click();
-            //code.Click();
-
-            driver.FindElement(By.Id("Code")).Clear();
-
-            //code.Clear();
-            driver.FindElement(By.Id("Code")).SendKeys("Hello world");
-            Thread.Sleep(2000);
-
-            driver.FindElement(By.Id("SaveButton")).Click();
-
-            Thread.Sleep(3000);
-
-            //IWebDriver check = (IWebDriver)driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[1]/td[1]"));
-
-           // IWebElement msg = driver.FindElement(By.XPath("//*[@id='tmsGrid]/div[3]/table/tbody/tr[1]/td[1]"));
-            //Console.WriteLine(msg.Displayed);
-            Console.Write("Edited Sucessfully");
-            //Console.Read();
-   
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-            Console.WriteLine("Check if the user is able to delete an existing time/ material record successfully");
-
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-            driver.FindElement(By.XPath("//*[@id='tmsGrid']/div[3]/table/tbody/tr[1]/td[5]/a[2]")).Click();
-            Console.Write("Deleted Sucessfully");
+            // Delete existing TM test
+            tmObj.DeleteTM(driver);
         }
     }
 }
+
